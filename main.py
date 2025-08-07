7a5ec6df2531c39473c39c9e8acfd990a5dba754
RamsMorales <rmuno@protonmail.com>
2025-08-07 12:01:57 -0400

    add max fps 60

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # Initializing the pygame modules
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+    # Defining the game screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    # Starting a clock object to introduce fps into the game
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.update()
-        
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
